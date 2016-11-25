ba1bb5a9ccb3c063b32f1ec1d8063b8edb9acc29
flycorn <ym1992it@gmail.com>
2016-11-25 18:19:42 +0800

    调整后台路由

--- app/Http/Routes/AdminRoute.php
+++ app/Http/Routes/AdminRoute.php
@@ -26,8 +26,8 @@ Route::group(['middleware' => ['admin.login', 'admin.menu', 'admin.auth']], func
     Route::get('manager/create', 'ManagerController@create')->name('admin.manager.create');
     Route::get('manager/{manager}/edit', 'ManagerController@edit')->name('admin.manager.edit');
     Route::get('manager/{manager}', 'ManagerController@show')->name('admin.manager.show');
-    Route::put('manager/{manager}', 'ManagerController@update')->name('admin.manager.edit');
     Route::post('manager', 'ManagerController@store')->name('admin.manager.create');
+    Route::put('manager/{manager}', 'ManagerController@update')->name('admin.manager.edit');
     Route::delete('manager/{manager}', 'ManagerController@destroy')->name('admin.manager.delete');
     //权限管理
     Route::get('permission', 'PermissionController@index')->name('admin.permission.index');
@@ -36,16 +36,17 @@ Route::group(['middleware' => ['admin.login', 'admin.menu', 'admin.auth']], func
     Route::get('permission/{permission}/create', 'PermissionController@create')->name('admin.permission.create');
     Route::get('permission/{permission}/show', 'PermissionController@show')->name('admin.permission.show');
     Route::get('permission/{permission}/edit', 'PermissionController@edit')->name('admin.permission.edit');
-    Route::put('permission', 'PermissionController@update')->name('admin.permission.edit');
     Route::post('permission', 'PermissionController@store')->name('admin.permission.create');
+    Route::put('permission/{permission}', 'PermissionController@update')->name('admin.permission.edit');
     Route::delete('permission/{permission}', 'PermissionController@destroy')->name('admin.permission.delete');
     //角色管理
     Route::any('role/{role}/auth', 'RoleController@auth')->name('admin.role.auth'); //角色授权
     Route::get('role', 'RoleController@index')->name('admin.role.index');
     Route::get('role/create', 'RoleController@create')->name('admin.role.create');
     Route::get('role/{role}', 'RoleController@show')->name('admin.role.show');
+    Route::get('role/{role}/edit', 'RoleController@edit')->name('admin.role.edit');
     Route::post('role', 'RoleController@store')->name('admin.role.create');
-    Route::put('role', 'RoleController@update')->name('admin.role.edit');
+    Route::put('role/{role}', 'RoleController@update')->name('admin.role.edit');
     Route::delete('role/{role}', 'RoleController@destroy')->name('admin.role.delete');
 
 
