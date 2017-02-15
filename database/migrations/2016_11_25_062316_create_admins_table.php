be6cee6c700173873ce408fa59b111eaea4f845c
flycorn <ym1992it@gmail.com>
2017-02-15 10:18:08 +0800

    更新数据表

--- database/migrations/2016_11_25_062316_create_admins_table.php
+++ database/migrations/2016_11_25_062316_create_admins_table.php
@@ -15,17 +15,16 @@ class CreateAdminsTable extends Migration
         //
         Schema::create('admins', function (Blueprint $table) {
             $table->engine = 'InnoDB';
-            $table->increments('user_id')->comment('用户id');
-            $table->string('username', 30)->default('')->comment('用户名');
+            $table->increments('user_id')->unique()->comment('用户id');
+            $table->string('username', 30)->unique()->default('')->comment('用户名');
             $table->string('nickname', 50)->default('')->comment('昵称');
             $table->string('avatar', 100)->default('upload/admin/avatar/default/avatar.png')->comment('头像');
-            $table->string('email', 30)->default('')->comment('邮箱');
+            $table->string('email', 30)->unique()->default('')->comment('邮箱');
             $table->string('password', 255)->default('')->comment('密码');
             $table->char('salt', 10)->default('')->comment('密码盐');
-            $table->string('remember_token')->default('')->comment('记住token');
+            $table->string('remember_token', 100)->unique()->default('')->comment('记住token');
             $table->string('introduction', 255)->default('')->comment('介绍');
             $table->timestamps();
-            $table->unique(['user_id', 'email', 'username', 'remember_token']);
         });
     }
 
