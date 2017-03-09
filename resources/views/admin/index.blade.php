78aee3b07827034cd2e18e04cc6f6d574d8f4612
flycorn <ym1992it@gmail.com>
2017-03-09 17:22:58 +0800

    切换新分支

--- resources/views/admin/index.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('admin.layouts.admin')
-
-@section('title', '主页')
-
-@section('content')
- <h1>Fc_Admin后台脚手架</h1>
-
-@endsection
--- resources/views/admin/layouts/mainFooter.blade.php
+++
@@ -1,8 +0,0 @@
-<footer class="main-footer">
-    <!-- To the right -->
-    <div class="pull-right hidden-xs">
-        @Flycorn
-    </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; 2016.</strong> All rights reserved.
-</footer>
